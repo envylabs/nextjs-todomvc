9836556fdca4962c45d12e47eaeea937c1fa671d
Nathaniel Bibler <nate@envylabs.com>
2022-02-19 11:01:29 -0500

    feat: set the static title in the layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import { FC } from 'react';
 import NewTodo from './new-todo';
 
 const Layout: FC = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Next.js • TodoMVC</title>
+      </Head>
       <section className="todoapp">
         <header className="header">
           <h1>todos</h1>
