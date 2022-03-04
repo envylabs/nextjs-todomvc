12da04795dfd5c73c1ee947d327d72367eb9bf78
Nathaniel Bibler <nate@envylabs.com>
2022-03-03 19:32:31 -0500

    feat: fill out /api/current-time endpoint behavior

--- pages/api/current-time.ts
+++ pages/api/current-time.ts
@@ -1,20 +1,50 @@
 import type { NextApiHandler } from 'next';
 import { getCurrentTime } from '../../models/world-time-api';
+import * as t from 'io-ts';
+import { isLeft } from 'fp-ts/lib/Either';
+import { optional, sparseType } from 'io-ts-extra';
+import prettyReporter from 'io-ts-reporters';
 
-const handler: NextApiHandler = async (_, res) => {
+const RequestPayload = sparseType({
+  timezone: optional(t.string),
+});
+
+const handler: NextApiHandler = async (request, response) => {
+  const { method } = request;
   let time: Date;
+  let timezone: string | null | undefined;
+  let parsedRequestBody: t.Validation<t.TypeOf<typeof RequestPayload>>;
+
+  if (method !== 'GET') {
+    response.setHeader('Allow', ['GET']);
+    response.status(405).end(`Method ${method} Not Allowed`);
+    return;
+  }
+
+  if (request.query) {
+    parsedRequestBody = RequestPayload.decode(request.query);
+
+    if (isLeft(parsedRequestBody)) {
+      response
+        .status(422)
+        .json({ error: prettyReporter.report(parsedRequestBody).join(', ') });
+      return;
+    }
+
+    timezone = parsedRequestBody.right.timezone;
+  }
 
   try {
-    time = await getCurrentTime();
+    time = await getCurrentTime(timezone);
   } catch (error) {
     console.error(error);
-    res.status(500).json({
+    response.status(500).json({
       error: error instanceof Error ? error.message : 'An error occurred',
     });
     return;
   }
 
-  res.status(200).json({ time });
+  response.status(200).json({ time });
 };
 
 export default handler;
