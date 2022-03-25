78d81e75223db239abcdf64839eddf9b641417b1
Nathaniel Bibler <nate@envylabs.com>
2022-03-25 09:33:35 -0400

    feat: add mock network server boot script for development

---
+++ mocks/cli.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env ts-node
+
+import { Name as ScenarioName } from './scenarios';
+
+import { createServer } from '.';
+
+const scenarioName: ScenarioName =
+  (process.argv[2] as ScenarioName) || 'staticTime';
+const hostname = 'localhost';
+const port = 3001;
+
+console.log(`Starting mock server with ${scenarioName}`);
+const server = createServer({ scenarioName });
+server.listen({ hostname, port }).then(
+  () => {
+    console.log(`Mock server is running on http://localhost:3001/.`);
+  },
+  () => {
+    console.error('Mock server failed to start.');
+  }
+);
+
+export {};
