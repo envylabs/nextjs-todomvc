cf8b21a1e3b22eff2b65ba9efa9c20f06908a0d9
Nathaniel Bibler <nate@envylabs.com>
2022-03-26 20:14:40 -0400

    test: add client-side network test

---
+++ cypress/integration/client-side-network.spec.ts
@@ -0,0 +1,32 @@
+describe('Client-Side Network', () => {
+  afterEach(() => {
+    cy.stopMockServers();
+  });
+
+  it('displays the current time from worldtimeapi.org', () => {
+    cy.create('worldTimeAPI', 'timezone', {
+      datetime: '2022-03-17T19:34:17.689501-04:00',
+      id: 'America/New_York',
+    });
+
+    cy.visit('/');
+    cy.Click('client-side request');
+    cy.contains('The time is').should(
+      'have.attr',
+      'datetime',
+      '2022-03-17T23:34:17.689Z'
+    );
+  });
+
+  it('displays the current time from worldtimeapi.org via a scenario', () => {
+    cy.scenario('staticTime');
+
+    cy.visit('/');
+    cy.Click('client-side request');
+    cy.contains('The time is').should(
+      'have.attr',
+      'datetime',
+      '2022-03-17T23:34:17.689Z'
+    );
+  });
+});
